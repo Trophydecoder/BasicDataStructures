c0f8e2b0a2bab59ad8a9ee6b3c343b9ae9e4d37e
Trophydecoder <trophydecoder@gmail.com>
2025-10-15 10:51:22 +0200

    insertion added comments

--- ArraySorting-InsertionSort.js.js
+++ ArraySorting-InsertionSort.js.js
@@ -1,4 +1,4 @@
-const array = [ 3 , 5 , 13 ,14 ,35 ,90]
+const array = [ 3 , 5 , 33 ,14 ,35 ,90]
 
 const insertionsort = array =>{
 
@@ -6,12 +6,14 @@ const insertionsort = array =>{
 
 
 for (let i = 1 ; i < len ; i++){
-    let current = array[i]
-    let previous = i - 1
-    while(previous >= 0 && array[previous] > current){
-        array[previous + 1] = array[previous]
-        previous -- 
+    let current = array[i]    //current index lets say its 5(based on unsorted array)
+    let previous = i - 1       //prevous one willl use to compare its 3 , i - 1 (based on index)
+   
+    while(previous >= 0 && array[previous] > current){ //while previous is bigger than current 
+        array[previous + 1] = array[previous]    //change the current to use the previous one
+        previous -- //change the current to use the current one
     }
+    
     array [previous + 1] = current
 }
 return array
